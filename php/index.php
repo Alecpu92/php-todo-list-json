aa6a434b82727324c25a9620ae3979047e4e23bc
Alecpu92 <alessandro_belfiore@hotmail.it>
2023-06-22 17:24:59 +0200

    aggiunta Access Control

--- php/index.php
+++ php/index.php
@@ -1,7 +1,10 @@
 <?php 
 
+header("Access-Control-Allow-Origin: http://127.0.0.1:5173");
+header("Access-Control-Allow-Headers: X-Requested-With");
+
 header('Content-Type: application/json');
 
 $listToDo = [
-    
+
 ]
