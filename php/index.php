e0c48e4d16aa4c826656dd59c2a40701db70da51
Alecpu92 <alessandro_belfiore@hotmail.it>
2023-06-23 16:42:23 +0200

    refactoring repo

--- php/index.php
+++ php/index.php
@@ -2,10 +2,13 @@
 
 header("Access-Control-Allow-Origin: http://localhost:5173/");
 header("Access-Control-Allow-Headers: X-Requested-With");
-
 header('Content-Type: application/json');
 
-$lists = [
+$listStr = file_get_contents("data.json");
+
+echo $listStr;
+
+[
     [
         'name' => 'HTML'
     ]
