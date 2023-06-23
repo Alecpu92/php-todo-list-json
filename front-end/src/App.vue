e415b3f3468a9f9ef836b166c86df647065dddf2
Alecpu92 <alessandro_belfiore@hotmail.it>
2023-06-23 16:42:54 +0200

    refactoring repo

---
+++ front-end/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+<h1>TO DO LIST</h1>
+<ul>
+  <li
+  v-for="(list, i) in lists"
+  :key="i"
+  >
+  {{ list.name }}
+
+</li>
+</ul>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+
+    return {
+      lists: []
+    };
+  },
+  mounted() {
+    
+    axios.get("http://loalhost/tmp/php/index.php")
+    .then(res => {
+
+      const data = res.data;
+      this.lists = data;
+    });
+  }
+
+}
+
+
+</script>
+
